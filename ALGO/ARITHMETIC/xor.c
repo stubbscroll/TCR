a80bb37311470bc3ff0e6b7522b6934f1f6c9c84
Ruben Spaans <spaans@pvv.org>
2012-05-21 22:31:15 +0200

    added countbits and sum-xor

---
+++ ALGO/ARITHMETIC/xor.c
@@ -0,0 +1,22 @@
+typedef long long ll;
+
+/*	xor of all numbers from 0 to n (inclusive)!
+		given n such that n%4==0, it's easy to see that
+		n^(n+1)^(n+2)^(n+3) is always 0:
+		xxxxxxx00
+		xxxxxxx01
+		xxxxxxx10
+		xxxxxxx11
+		the bit pattern for the x's is equal for all, and is 0. bit 0 and 1
+		are set an even number of times.
+*/
+/*	return xor of all numbers from 0 to n, inclusive */
+/*	OK Topcoder SRM 543 div 1 250, 21.05.2012 */
+ll sumxor(ll n) {
+	switch(n&3) {
+	case 0: return n;
+	case 1: return 1;
+	case 2: return n+1;
+	}
+	return 0;
+}
