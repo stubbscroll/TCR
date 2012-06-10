c59fb536c6afe44a341e0a98fc81910d43c4a71c
Ruben Spaans <spaans@pvv.org>
2012-06-10 10:49:40 +0200

    added todo problem for countbits

--- ALGO/ARITHMETIC/countbits.c
+++ ALGO/ARITHMETIC/countbits.c
@@ -1,7 +1,8 @@
 typedef long long ll;
-/*	count the number of bits set in bit position i for all number from
-		0 to n, inclusive. should work with b<=61, or b<=62 for ull */
-/*	OK Topcoder SRM 543 div 1 250 21.05.2012, NB only bit 0 in answer tested */
+/* count the number of bits set in bit position i for all number from
+   0 to n, inclusive. should work with b<=61, or b<=62 for ull */
+/* OK Topcoder SRM 543 div 1 250 21.05.2012, NB only bit 0 in answer tested */
+/* TODO sjekk 12208 */
 ll countbits(ll n,int b) {
 	ll x=(n+1)>>(b+1)<<b;
 	ll y=(n+1)&((1LL<<(b+1))-1);
