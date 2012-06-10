81be992cdf50990a9184cf24517328ece93940a5
Ruben Spaans <spaans@pvv.org>
2012-06-10 10:51:53 +0200

    solved more with fenwick

--- ALGO/DATASTRUCTURE/bittree.c
+++ ALGO/DATASTRUCTURE/bittree.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-/*  binary index (fenwick) tree code, 1-indexed tree[] */
-/*  store frequencies f[1]..f[n]
-    update frequency f[i] in O(log n)
-    read cumulative frequency sum i=1 to k f[i] in O(log n)
-    find kth smallest element in O(log n)
-    read single frequency by doing read(i)-read(i-1)
-    set frequency f[i]=a by doing update(a-read(i),i))
-    delete element: update(-1,ix) */
-/*  WARNING: don't use index 0. this case can happen if you want
-    to count values in a range. instead, shift the range. */
-/*  OK IDI Open 2010 F Beads ??.??.2011 */
-/*  OK Topcoder SRM 310 div 1 500 worst case 69 ms, 03.10.2011 */
-/*  OK UVa 501 n<=30000, 0.164 seconds, 14.01.2012 */
-/*  OK UVa 10909 n<=1100000, 0.168 seconds, 04.10.2011 */
-/*  OK UVa 11525 n<=50000, 0.152 seconds, 04.10.2011 */
-/*  OK UVa 12365 n<=100000, 0.488 seconds, 12.11.2011 */
-/*  OK NCPC 2011 problem B Mega-inversions 01.10.2011 */
-/*  OK SPOJ 9889 (NWERC 2011 problem C movie collection) n<=200000 0.95 seconds 15.01.2012 */
+/* binary index (fenwick) tree code, 1-indexed tree[] */
+/* store frequencies f[1]..f[n]
+   update frequency f[i] in O(log n)
+   read cumulative frequency sum i=1 to k f[i] in O(log n)
+   find kth smallest element in O(log n)
+   read single frequency by doing read(i)-read(i-1)
+   set frequency f[i]=a by doing update(a-read(i),i))
+   delete element: update(-1,ix) */
+/* WARNING: don't use index 0. this case can happen if you want
+   to count values in a range. instead, shift the range. */
+/* OK IDI Open 2010 F Beads ??.??.2011 */
+/* OK Topcoder SRM 310 div 1 500 worst case 69 ms, 03.10.2011 */
+/* OK UVa 501 n<=30000, 0.164 seconds, 14.01.2012 */
+/* OK UVa 10015 n<=3501, 0.356 seconds, 08.06.2012 */
+/* OK UVa 10057 n<=65537, 0.096 seconds, 08.06.2012 */
+/* OK UVa 10909 n<=1100000, 0.168 seconds, 04.10.2011 */
+/* OK UVa 11525 n<=50000, 0.152 seconds, 04.10.2011 */
+/* OK UVa 12365 n<=100000, 0.488 seconds, 12.11.2011 */
+/* OK NCPC 2011 problem B Mega-inversions 01.10.2011 */
+/* OK SPOJ 9889 (NWERC 2011 problem C movie collection) n<=200000 0.95 seconds 15.01.2012 */
 int tree[100001],n;   /*  warning, array must be of size n+1 */
 int leftmostbit;  /*  cache the leftmost set bit in n here! */
 /*  initialize structure with n elements */
