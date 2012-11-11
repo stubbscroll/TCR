3f09b1272e5df0f804253a95bb38bd6eb9b7d692
stubbscroll <Ruben Spaans@.(none)>
2012-11-11 20:00:13 +0100

    added 64-bits curl example

---
+++ LIB/CURL64/webpage.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
+#define BUF 1000000
+char buffer[BUF];
+int bptr;
+
+/*  need to collect everything in one huge buffer, as data can be split in the
+    middle of a line */
+size_t webline(void *ptr,size_t size,size_t n,void *userdata) {
+  int i;
+  char *t=(char *)ptr;
+  for(i=0;t[i];i++) buffer[bptr++]=t[i];
+  buffer[bptr]=0;
+  return n;
+}
+
+void loadwebpage(char *url) {
+  CURL *curl=curl_easy_init();
+  CURLcode res;
+  curl_easy_setopt(curl,CURLOPT_URL,url);
+  curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,webline);
+  bptr=0;
+  res=curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+}
+
+/*  small example showing how to open a webpage */
+int main() {
+  loadwebpage("http://www.pvv.org/~spaans/");
+  puts(buffer);
+  return 0;
+}
