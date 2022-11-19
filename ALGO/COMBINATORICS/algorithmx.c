8cf9f9352102cde0d2aed13c5d7885d0b91892c4
stubbscroll <stubbscroll@superruben.com>
2022-11-19 03:03:45 +0100

    algorithm x: added support for secondary items

--- ALGO/COMBINATORICS/algorithmx.c
+++ ALGO/COMBINATORICS/algorithmx.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-// algorithm x (dancing links) for finding exact cover
+// algorithm x (dancing links) for finding exact cover.
+// supports secondary items that can be covered <=1 times (but not twice),
+// items that aren't included in the horizontal doubly linked list (llink/rlink)
+// become secondary.
 // source: the art of computer programming volume 4b (donald knuth)
 
 // 2022-11-18: langford(n) correct up to 16
+// 2022-11-19: n-queens 
 
 #define MAXITEM 1000
 #define MAX 10000
 
-int name[MAXITEM];
+int name[MAXITEM];     // optional, name each item
 int llink[MAXITEM];
 int rlink[MAXITEM];
 int top[MAX],*len=top;
@@ -57,9 +61,9 @@ void visit(int l) {
 	solutions++;
 }
 
-// n: number of items (items are in indexes 1..n, so pass n+1) (except i removed n)
-// z: index of last spacer (actually not used currently)
-void algorithmx(int z) {
+// number of items not passed, can be deduced from data structures,
+// for example in llink[0]
+void algorithmx() {
 	int l=0;
 x2:
 	if(rlink[0]==0) {
@@ -109,17 +113,25 @@ x8:
 
 // helper functions for building the data structure ///////////////////////////
 
-int lastd[MAXITEM];
 int spacerid;
 int nextpos;
+int numitems;
 
-// n: number of items
-void inithelp(int n) {
+// n: total number of items (primary + secondary)
+// m: number of secondary items
+// primary items appear before secondary ones in the data structures
+void inithelp(int n,int m) {
 	if(n>=MAXITEM) printf("error, increase MAXITEM and recompile\n");
+	numitems=n;
+	int p=n-m; // primary items
 	for(int i=1;i<=n;i++) len[i]=0;
-	for(int i=0;i<=n;i++) llink[i]=(i+n)%(n+1);
-	for(int i=0;i<=n;i++) rlink[i]=(i+1)%(n+1);
-	for(int i=1;i<=n;i++) lastd[i]=i; // next index for setting dlink for given item
+	for(int i=0;i<=p;i++) llink[i]=(i+p)%(p+1);
+	for(int i=0;i<=p;i++) rlink[i]=(i+1)%(p+1);
+	for(int i=1;i<=n;i++) dlink[i]=ulink[i]=i;
+	// let secondary items form its own doubly linked list
+	for(int i=p+1;i<=n;i++) llink[i]=i-1;
+	for(int i=p+1;i<=n;i++) rlink[i]=i+1;
+	if(n>p) llink[p+1]=n,rlink[n]=p+1;
 	nextpos=n+1;
 	spacerid=0;
 	top[nextpos++]=spacerid--;
@@ -131,26 +143,19 @@ void addrow(int m,int *a) {
 	int lastspacer=nextpos-1;
 	for(int i=0;i<m;i++) {
 		int ix=a[i];
+		if(ix<1 || ix>numitems) printf("illegal item number %d, max is %d\n",ix,numitems);
 		len[ix]++;
-		ulink[nextpos]=lastd[ix];
-		dlink[lastd[ix]]=nextpos;
-		lastd[ix]=nextpos;
-		top[nextpos++]=ix;
+		top[nextpos]=ix;
+		dlink[nextpos]=ix;
+		ulink[nextpos]=ulink[ix];
+		dlink[ulink[ix]]=nextpos;
+		ulink[ix]=nextpos;
+		nextpos++;
 	}
 	// add trailing spacer
 	dlink[lastspacer]=nextpos-1;
 	ulink[nextpos]=lastspacer+1;
 	top[nextpos++]=spacerid--;
-}
-
-// finalize data structure, return index to last spacer
-// n: number of items
-int finishhelp(int n) {
-	for(int i=1;i<=n;i++) {
-		ulink[i]=lastd[i];
-		dlink[lastd[i]]=i;
-	}
-	return nextpos;
 }
 
 // tests! /////////////////////////////////////////////////////////////////////
@@ -163,21 +168,17 @@ int finishhelp(int n) {
 void langford(int n) {
 	// we have 3n items to be covered:
 	// numbers 1..n and slots 1..2n to be filled with numbers
-	inithelp(3*n);
+	inithelp(3*n,0);
 	// construct rows
 	int row[3];
 	for(int i=1;i<=n;i++) for(int j=0;j<2*n-i-1;j++) {
-//		printf("add %d %d %d\n",i,j,j+i+1);
 		row[0]=i;
 		row[1]=n+j+1;
 		row[2]=n+j+i+2;
 		addrow(3,row);
 	}
-	int lastspacer=finishhelp(3*n);
-//	for(int i=0;i<=3*n;i++) printf("%d: llink %d, rlink %d, len %d, ulink %d, dlink %d\n",i,llink[i],rlink[i],len[i],ulink[i],dlink[i]);
-//	for(int i=0;i<lastspacer;i++) printf("%d: top %d, ulink %d, dlink %d\n",i,top[i],ulink[i],dlink[i]);
 	solutions=0;
-	algorithmx(lastspacer);
+	algorithmx();
 	printf("langford(%d): found %lld solutions\n",n,solutions);
 }
 
@@ -185,7 +186,34 @@ void langford_all() {
 	for(int i=3;i<=12;i++) if(i%4==0 || i%4==3) langford(i);
 }
 
+// find all ways to place n queens on a n*n chessboard
+void nqueens(int n) {
+	// items: 6n-2 items total, 2n primary, 4n-2 secondary
+	// 1 to n: n rows
+	// n-1 to 2n: n columns
+	// 2n+1 to 4n-1: 2n-1 diagonals from lower left to upper right (secondary)
+	// 4n to 6n-2: 2n-1 diagonals from upper left to lower right (secondary)
+	inithelp(6*n-2,4*n-2);
+	int row[4];
+	for(int x=1;x<=n;x++) for(int y=1;y<=n;y++) {
+		// option: place queen on (x,y)
+		row[0]=y;
+		row[1]=n+x;
+		row[2]=2*n+x+y-1;
+		row[3]=4*n-1+x-y+n;
+		addrow(4,row);
+	}
+	solutions=0;
+	algorithmx();
+	printf("nqueens(%d): found %lld solutions\n",n,solutions);
+}
+
+void nqueens_all() {
+	for(int i=1;i<=14;i++) nqueens(i);
+}
+
 int main() {
 	langford_all();
+	nqueens_all();
 	return 0;
 }
